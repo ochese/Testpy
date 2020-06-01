22631a02723fdd86ae28e1a991f8e5489afc95f6
ochese <ochese_95@hotmail.com>
2020-06-01 14:12:54 -0500

    First commit
    prueba de primer commit

---
+++ Tesis - 01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 00:06:14 2020
+
+@author: Jose
+"""
+
+import pandas as pd
+
+df = pd.read_excel('C:\\Users\\Jose\\Downloads\\form_python.xlsx')
+df.head(10)#dxdcdxs
+
